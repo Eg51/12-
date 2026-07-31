8619468e3b29611fb85dbc8a6211b788c2491777
Your Name <dciphererz@gmail.com>
2026-07-31 23:39:30 +0100

    finally fixed the build bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,105 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import localFont from "next/font/local";
+import "./globals.css";
 
 const poppins = localFont({
   src: [
     {
-      path: "../public/fonts/Poppins-Regular.woff2",
+      path: "../public/fonts/Poppins-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Poppins-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Poppins-ExtraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Poppins-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/fonts/Poppins-Bold.woff2",
+      path: "../public/fonts/Poppins-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Poppins-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Poppins-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-Bold.ttf",
       weight: "700",
       style: "normal",
     },
-      {
-      path: "../public/fonts/Poppins-Regular.woff2",
-      weight: "400",
+    {
+      path: "../public/fonts/Poppins-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-ExtraBold.ttf",
+      weight: "800",
       style: "normal",
     },
-    // Add more weights as needed
+    {
+      path: "../public/fonts/Poppins-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Poppins-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Poppins-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
   ],
   variable: "--font-poppins",
   display: "swap",
 });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
