60152182dff8500bf451c204aa02a7d0f2ab847d
Your Name <dciphererz@gmail.com>
2026-08-18 00:20:56 +0100

    mmmm

--- app/Log-in/page.tsx
+++ app/Log-in/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Logge from '@/app/components/Logge'
+// import React from 'react'
+// import Logge from '@/app/components/Logge'
 
 
-const page = () => {
-  return (
-    <Logge/>
-  )
+// const page = () => {
+//   return (
+//     <Logge/>
+//   )
+// }
+
+// export default page
+export default function LoginPage() {
+  return <div>Login page works!</div>;
 }
-
-export default page
