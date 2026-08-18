0f96dc2d977d6019336256dd5cd5896bb6b2d1f2
Your Name <dciphererz@gmail.com>
2026-08-18 12:26:37 +0100

    update changes to /Dashbord page.tsx

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -13,7 +13,7 @@ export default function DashboardPage() {
       <DesktopNav />
       <Dash />
       <ChatWidgett />
-      <Iconpack />
+      <div className="p-5"><Iconpack /></div>
     </>
   );
 }
